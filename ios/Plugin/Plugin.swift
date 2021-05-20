38f8d60dab5b4a9ecd10efcde4f5a823b41aac18
Austen Mack <austen.mack@asu.edu>
2021-05-20 14:07:11 -0700

    updates ios/Plugin.swift to Ray Wenderlich implementation

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -9,6 +9,15 @@ import AVFoundation
 @objc(Mixer)
 public class Mixer: CAPPlugin {
     
+    public let engine: AVAudioEngine = AVAudioEngine()
+    public let player: AVAudioPlayerNode = AVAudioPlayerNode()
+    public var audioFile: AVAudioFile?
+    public var audioSampleRate: Double = 0
+    public var audioLengthSeconds: Double = 0
+    public var audioLengthSamples: AVAudioFramePosition = 0
+    public var needsFileScheduled = true
+    public var seekFrame: AVAudioFramePosition = 0
+    
     public override func load() {
         super.load()
 //        do {
@@ -30,81 +39,82 @@ public class Mixer: CAPPlugin {
     }
     
     @objc func play(_ call: CAPPluginCall) {
-        do {
-            let session = AVAudioSession.sharedInstance()
-            try session.setCategory(AVAudioSession.Category.playAndRecord, options: [.mixWithOthers, .defaultToSpeaker])
-            
-            try session.setActive(true)
+        let queue = DispatchQueue(label: "com.getcapacitor.community.audio.complex.queue",
+                                  qos: .userInitiated)
+        queue.async {
+            self.setupAudio()
         }
-        catch {
-            // TODO: handle this error
-        }
-        let filePath = call.getString("filePath") ?? ""
-        NSLog("initialized filepath to " + filePath)
-        if (filePath == "") {
-            call.error("You need a filepath, bub.")
-        }
-        let fileURL = NSURL.fileURL(withPath: filePath)
-        NSLog("initialized file URL to " + fileURL.absoluteString)
-        let audioEngine = AVAudioEngine.init()
-        let audioMixer = AVAudioMixerNode()
-        let audioPlayerNode: AVAudioPlayerNode! = AVAudioPlayerNode.init()
-        NSLog("right before audio format")
-//        let audioFormat = audioPlayerNode.inputFormat(forBus: 0)
-        NSLog("right after audio format")
-        var audioPlayerFile = AVAudioFile()
-        NSLog("initialized engine, mixer, player node")
+    }
+    
+    private func setupAudio() {
+      // 1
+      guard let fileURL = Bundle.main.url(
+              forResource: "KnewSomething",
+              withExtension: "mp3")
+      else {
+        return
+      }
+      
+      // 2
+      do {
+        let file = try AVAudioFile(forReading: fileURL)
+        let format = file.processingFormat
         
-        audioEngine.attach(audioPlayerNode)
-        audioEngine.attach(audioMixer)
-        NSLog("attached the stuff")
+        audioLengthSamples = file.length
+        audioSampleRate = format.sampleRate
+        audioLengthSeconds = Double(audioLengthSamples) / audioSampleRate
         
-        do {
-            audioPlayerFile = try AVAudioFile.init(forReading: fileURL)
-            
+        audioFile = file
+        
+        // 3
+        configureEngine(with: format)
+      } catch {
+        print("Error reading the audio file: \(error.localizedDescription)")
+      }
+    }
+    
+    private func configureEngine(with format: AVAudioFormat) {
+      // 1
+      engine.attach(player)
+      
+      // 2
+      engine.connect(player, to: engine.mainMixerNode, format: format)
+      engine.prepare()
+      
+      do {
+        // 3
+        try engine.start()
+        
+        scheduleAudioFile()
+//        isPlayerReady = true
+      } catch {
+        print("Error starting the player: \(error.localizedDescription)")
+      }
+    }
+    
+    private func scheduleAudioFile() {
+      guard let file = audioFile, needsFileScheduled else {
+        return
+      }
+      
+      needsFileScheduled = false
+      seekFrame = 0
+      
+      player.scheduleFile(file, at: nil) {
+        self.needsFileScheduled = true
+      }
+    }
+    
+    private func playOrPause() {
+        if player.isPlaying {
+          // 2
+          player.pause()
+        } else {
+          // 3
+          if needsFileScheduled {
+            scheduleAudioFile()
+          }
+          player.play()
         }
-        catch {
-            call.error("Could not initialize audio file")
-        }
-        let audioFormat = audioPlayerFile.processingFormat
-        let audioFrameCount = UInt32(audioPlayerFile.length)
-        let audioFileBuffer = AVAudioPCMBuffer(pcmFormat: audioFormat, frameCapacity: audioFrameCount)
-        do {
-            try audioPlayerFile.read(into: audioFileBuffer!)
-        }
-        catch {
-            call.error("Could not read into FileBuffer")
-        }
-        
-        audioEngine.connect(audioMixer, to: audioEngine.mainMixerNode, format: nil)
-        audioEngine.connect(audioPlayerNode, to: audioMixer, format: audioFileBuffer!.format)
-        NSLog("connected the stuff")
-        
-        audioPlayerNode.scheduleBuffer(audioFileBuffer!, at: nil, completionHandler: nil)
-        NSLog("Just scheduled your file")
-        
-        audioEngine.prepare()
-        do {
-            try audioEngine.start()
-            audioPlayerNode.volume = 1
-            audioPlayerNode.play()
-        }
-        catch {
-            call.error("Could not start audio engine")
-        }
-        
-
-        
-// This is an example of how to record to a file:
-// Possibly could be used to write out to a stream??????
-//        if let audioURL = audioURL {
-//            do {
-//                self.recordedOutputFile = try AVAudioFile(forWriting: audioURL, settings: audioMixer.outputFormat(forBus: 0).settings)
-//            }
-//            catch {
-//                // TODO: handle the error
-//            }
-//        }
-        
     }
 }
