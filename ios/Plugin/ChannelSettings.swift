dab82cbe168f75ec1bab3f1e33520d1b69952766
Austen Mack <austen.mack@asu.edu>
2021-06-18 15:31:29 -0700

    added mic methods and metering

---
+++ ios/Plugin/ChannelSettings.swift
@@ -0,0 +1,9 @@
+//
+//  ChannelSettings.swift
+//  Plugin
+//
+//  Created by Austen Mack on 6/16/21.
+//  Copyright © 2021 Max Lynch. All rights reserved.
+//
+
+import Foundation
---
+++ ios/Plugin/MicInput.swift
@@ -0,0 +1,9 @@
+//
+//  MicInput.swift
+//  Plugin
+//
+//  Created by Austen Mack on 6/11/21.
+//  Copyright © 2021 Max Lynch. All rights reserved.
+//
+
+import Foundation
